c5684060f05bd781819ea1b93556eeb77ea47ad5
rushi-ace-01 <144647020+rushi-ace-01@users.noreply.github.com>
2024-08-28 17:17:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,7 @@ with st.expander('Data'):
   
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+#Data preprations
+with st.sidebar:
+  st.hewader('Input Features')
