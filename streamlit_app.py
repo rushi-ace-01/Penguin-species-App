0c58de298a6859bde661cda796f2c5d8cc9284a9
rushi-ace-01 <144647020+rushi-ace-01@users.noreply.github.com>
2024-08-28 21:45:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,12 @@ with st.expander('Data'):
   df
   
   st.write('**X**')
-  X= df.drop('species', axis=1)
-  X
+  X_raw= df.drop('species', axis=1)
+  X_raw
 
   st.write('**y**')
-  y= df.species
-  y
+  y_raw = df.species
+  y_raw
   
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
@@ -41,13 +41,20 @@ data = {'island': island,
          'flipper_length_mm': flipper_length_mm}
          
 input_df= pd.DataFrame(data, index=[0])
-input_penguins = pd.concat([input_df, X], axis=0)
+input_penguins = pd.concat([input_df, X_raw], axis=0)
 
-#Encode
+#Encode X
 encode= ['island','sex']
 df_penguins= pd.get_dummies(input_penguins, prefix= encode)
 input_row= df_penguins[:1]
 
+#Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo':2}
+def target_encode(val):
+  return target_mapper(val)
+          
 with st.expander('Input features'):
   st.write('**Input Penguin**')
   input_df
