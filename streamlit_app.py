d7c6237c39c7e17e1cb2122b029efaeb872979df
rushi-ace-01 <144647020+rushi-ace-01@users.noreply.github.com>
2024-08-28 17:36:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,5 +29,5 @@ with st.sidebar:
   gender= st.selectbox('Gender', ('Male', 'Female'))
   bill_length_mm= st.slider('Bill length (mm)', 39.1 , 59.6,43.9)
   bill_depth_mm= st.slider('Bill depth (mm)', 172.0, 231.0, 201.0)
-  body_mass_g= st.slider('Body mass(g) , 2700.0, 6300.0, 4207.0)
+  body_mass_g= st.slider('Body mass(g)' , 2700.0, 6300.0, 4207.0)
   
