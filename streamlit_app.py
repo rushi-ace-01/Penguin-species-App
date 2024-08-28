9b3aa3232ae00c554e7009bda7f5af32a8f99359
rushi-ace-01 <144647020+rushi-ace-01@users.noreply.github.com>
2024-08-28 10:04:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ import pandas as pd
 st.title('🤖 Machine Learning App')
 
 st.info('This app predicts outcomes based on machine learning model')
+
+df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
