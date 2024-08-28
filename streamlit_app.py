4b2986b20ba0e331bcc6528b7d9f1d7c69165082
rushi-ace-01 <144647020+rushi-ace-01@users.noreply.github.com>
2024-08-28 17:36:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,7 @@ with st.sidebar:
   #"island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
   island= st.selectbox('Island', ('Torgersen', 'Biscoe', 'Dream'))
   gender= st.selectbox('Gender', ('Male', 'Female'))
-  bill_length= st.slider('Bill_length_mm', 39.1 , 59.6,43.9)
+  bill_length_mm= st.slider('Bill length (mm)', 39.1 , 59.6,43.9)
+  bill_depth_mm= st.slider('Bill depth (mm)', 172.0, 231.0, 201.0)
+  body_mass_g= st.slider('Body mass(g) , 2700.0, 6300.0, 4207.0)
+  
