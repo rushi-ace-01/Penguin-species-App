748e6877bc79d9cb82d7d928e6d0cb79d1a47099
rushi-ace-01 <144647020+rushi-ace-01@users.noreply.github.com>
2024-08-28 21:56:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ def target_encode(val):
 y= y_raw.apply(target_encode)
 y
           
-with st.expander('Data preparation):
+with st.expander('Data preparation'):
   st.write('**Encoded X (input penguin)**')
   input_row
   st.write('**Encoded y**')
