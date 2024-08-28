fe28793a4e1cb9f85968c43ba0b875cdb9e98360
rushi-ace-01 <144647020+rushi-ace-01@users.noreply.github.com>
2024-08-28 20:49:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ with st.sidebar:
          'body_mass_g': body_mass_g, 
          'gender': gender}
          
-  input_df= pd.Dataframe(data, index[0])   
+  input_df= pd.DataFrame(data, index[0])   
   input_df
          
          
